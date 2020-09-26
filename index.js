57cdb50de6ee7fa0f9de24efb197fcbbb9c1fbb9
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-26 00:48:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+console.log(dodger.style.left); 
+console.log(dodger.style.bottom);
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown",
+function (e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+  }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = 
+  dodger.style.left.replace("px","");
+  let left = parseInt(leftNumbers, 10);
+  
+  if (left > 0) {
+    dodger.style.left =  `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
